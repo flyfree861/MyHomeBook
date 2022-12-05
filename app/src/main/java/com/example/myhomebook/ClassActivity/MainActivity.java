08371ac2a48844df5869585b5c0302615b3ce98e
flyfree861 <mazzuoccolo.fabio@gmail.com>
2022-12-05 10:41:38 +0100

    MyHomeBook First creation

--- app/src/main/java/com/example/myhomebook/ClassActivity/MainActivity.java
+++ app/src/main/java/com/example/myhomebook/ClassActivity/MainActivity.java
@@ -1,17 +1,59 @@
 package com.example.myhomebook.ClassActivity;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.content.ContentValues.TAG;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.example.myhomebook.R;
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.identity.BeginSignInResult;
+import com.google.android.gms.auth.api.identity.Identity;
+import com.google.android.gms.auth.api.identity.SignInClient;
+import com.google.android.gms.auth.api.identity.SignInCredential;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
+    //UI Object declaration
+    AppCompatButton btnLoginMail;
+    ImageButton btnLoginGoogle;
+    TextInputEditText txtMail, txtPassword;
+
+    //Google oAuth Object declaration
+    BeginSignInRequest signInRequest;
+    private static final int REQ_ONE_TAP = 2;  // Can be any integer unique to the Activity.
+    private boolean showOneTapUI = true;
+    SignInClient oneTapClient;
+
+
+    //Firebase
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -22,6 +64,87 @@ public class MainActivity extends AppCompatActivity
         w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         setContentView(R.layout.activity_main);
 
+        //UI Association
+        btnLoginGoogle = findViewById(R.id.btn_login_with_google);
+        btnLoginMail   = findViewById(R.id.btn_login_with_mail);
+
+        btnLoginGoogle.setOnClickListener(this);
+        btnLoginMail.setOnClickListener(this);
+
+        //Google one tap and signin
+        signInRequest = BeginSignInRequest.builder()
+                .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                        .setSupported(true)
+                        // Your server's client ID, not your Android client ID.
+                        .setServerClientId(getString(R.string.default_web_client_id))
+                        // Only show accounts previously used to sign in.
+                        .setFilterByAuthorizedAccounts(true)
+                        .build())
+                .build();
+
+        //Firebase
+
+}
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQ_ONE_TAP:
+                try {
+
+                    SignInCredential credential = oneTapClient.getSignInCredentialFromIntent(data);
+                    String idToken = credential.getGoogleIdToken();
+                    if (idToken !=  null) {
+                        // Got an ID token from Google. Use it to authenticate
+                        // with Firebase.
+                        Toast.makeText(this, "Got ID token.", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (ApiException e) {
+                    // ...
+                }
+                break;
+        }
+    }
+
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        if(view.getId() == R.id.btn_login_with_google)
+        {
+          btnGoogleClick();
+        }
+
+        else if(view.getId() == R.id.btn_login_with_mail)
+        {
+
+        }
+    }
+
+    public void btnGoogleClick()
+    {
+
+    }
+
+    private void updateUI(FirebaseUser currentUser)
+    {
+        if(currentUser != null)
+        {
+            Toast.makeText(this, currentUser.toString(), Toast.LENGTH_SHORT).show();
+        }
 
     }
 }
+
+
